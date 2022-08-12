81a7512ea6aa7c49302427155ef1e85670121947
KirylMikheyeu <40001510+KirylMikheyeu@users.noreply.github.com>
2022-08-12 15:54:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ module.exports = function check(str, bracketsConfig) {
   };
 
   const st = [];
-  for (let i = 0; i < s.length; i++) {
-    if (isClosedBracket(s[i])) {
-      if (brackets[s[i]] !== st.pop()) return false;
+  for (let i = 0; i < str.length; i++) {
+    if (isClosedBracket(str[i])) {
+      if (brackets[str[i]] !== st.pop()) return false;
     } else {
-      st.push(s[i]);
+      st.push(str[i]);
     }
   }
   return st.length === 0;
