7901fdef6145f34cf156b2d2aedc9c91f392220b
KirylMikheyeu <40001510+KirylMikheyeu@users.noreply.github.com>
2022-08-12 15:59:33 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  const brackets = {
-    ")": "(",
-    "]": "[",
-    "}": "{",
-    "|":"|"
-  };
-
-  const st = [];
-  for (let i = 0; i < str.length; i++) {
-    if (isClosedBracket(str[i])) {
-      if (brackets[str[i]] !== st.pop()) return false;
-    } else {
-      st.push(str[i]);
-    }
+    str = str.split('');
+  for (let i = 0; i <= str.length; i++) {
+    bracketsConfig.map(bracket => {
+      if (str[i] === bracket[0] && str[i + 1] === bracket[1]) {
+        str.splice(i, 2);
+        i = i - 2;
+      }
+    });
   }
-  return st.length === 0;
-}
 
-function isClosedBracket(ch) {
-  return [")", "]", "}", "|"].indexOf(ch) > -1;
+  if (str.length !== 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
-}
-
